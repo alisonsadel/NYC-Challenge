38f2465d0fd857e05076a3f81dd3cf4d5d3c060c
Shadee Tabasi <shadeetabasi@gmail.com>
2021-04-27 16:49:20 -0400

    added template files

---
+++ python_files/Resources/app2.py
@@ -0,0 +1,10 @@
+# Set up Dependencies 
+import numpy as np
+import pandas as pd
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from flask import Flask, jsonify, render_template, redirect
+import json
+from pprint import pprint
